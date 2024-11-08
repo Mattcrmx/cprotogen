689083c6ba31d3f66f28c65964e873a3e61a9e9d
MattCrmx <matthias.cremieux@hotmail.fr>
2024-11-08 16:48:29 +0100

    :recycle: :white_check_mark: :sparkles: handle empty header and add tests

--- src/cprotogen/generator.py
+++ src/cprotogen/generator.py
@@ -51,6 +51,9 @@ class SourceVisitor(BaseVisitor):
         self.last_proto_line_no = max(self.last_proto_line_no, node.decl.coord.line)
 
         name = node.decl.name
+        if name == "main":
+            # we don't want to prototype main
+            return
 
         return_type = self._generator.visit(node.decl.type.type)
 
@@ -58,15 +61,25 @@ class SourceVisitor(BaseVisitor):
         if node.decl.type.args:
             for param in node.decl.type.args.params:
                 param_type = self._generator.visit(param.type)
+                param_name = param.name if param.name else ""
 
-                # let's collate the pointer to the variable name
-                params.append(
-                    f"{param_type} {param.name}"
-                    if not param_type.endswith("*")
-                    else f"{param_type}{param.name}"
-                )
+                # handle function pointer parameters
+                if isinstance(param.type, c_ast.PtrDecl) and isinstance(
+                    param.type.type, c_ast.FuncDecl
+                ):
+                    func_ptr = self._generator.visit(param.type)
+                    # parameter name back into the function ptr declaration
+                    func_ptr = func_ptr.replace("(*)", f"(*{param_name})")
+                    params.append(func_ptr)
+                else:
+                    # regular parameters
+                    parameter = param_type
+                    # special formatting for pointers
+                    parameter += " " if param_name and not param_type.endswith("*") else ""
+                    parameter += param_name
+                    params.append(parameter)
 
-        param_str = ", ".join(params)
+        param_str = ", ".join(params) if len(params) != 0 else "void"
 
         # nice formatting
         proto = f"{return_type} " if not return_type.endswith("*") else return_type
@@ -126,6 +139,20 @@ def _merge_prototypes(header_visitor: HeaderVisitor, src_visitor: SourceVisitor)
     return final_protos
 
 
+def _validate_header_structure(
+    source_filepath: str, header_filepath: str, existing_prototypes: list[str]
+) -> None:
+    with open(header_filepath) as f:
+        header = f.read().strip()
+
+    if not header or len(existing_prototypes) == 0:
+        # The header file is empty or there are no existing prototypes
+        # Generate a new header structure
+        _generate_header(
+            source_filepath=source_filepath, header_filepath=header_filepath, prototypes=[]
+        )
+
+
 def _generate_header(source_filepath: str, header_filepath: str, prototypes: list[str]) -> None:
     """Generate a header file from the prototypes.
 
@@ -135,10 +162,14 @@ def _generate_header(source_filepath: str, header_filepath: str, prototypes: lis
         prototypes: the function prototypes
     """
     # we use a classic header name here
-    header_name = source_filepath.split("/")[-1].replace(".c", "_H").upper()
+    header_name = os.path.basename(source_filepath).replace(".c", "_H").upper()
     header = f"#ifndef {header_name}\n#define {header_name}\n\n"
-    header += f"{''.join(prototypes)}\n"
-    header += f"#endif // {header_name}"
+
+    if prototypes:
+        header += "".join(prototypes)
+        header += "\n"  # Add a newline after the prototypes
+
+    header += f"\n#endif // {header_name}\n"  # Add newlines before and after the endif
 
     with open(header_filepath, "w") as f:
         f.write(header)
@@ -147,30 +178,41 @@ def _generate_header(source_filepath: str, header_filepath: str, prototypes: lis
 def _update_header(
     header_filepath: str, header_visitor: HeaderVisitor, src_visitor: SourceVisitor
 ) -> None:
-    """Updates a header file with the changed prototypes.
-
-    Args:
-        header_filepath: the header filepath
-        header_visitor: the visitor object for the header file
-        src_visitor: the visitor object for the source file
-    """
-    first_header_proto_idx = header_visitor.first_proto_line_no - 1  # line count starts at 1
+    """Updates a header file with the changed prototypes."""
     protos = _merge_prototypes(header_visitor, src_visitor)
 
-    nb_old_protos = header_visitor.last_proto_line_no - header_visitor.first_proto_line_no + 1
-
-    # to avoid overriding things that happen to be declared after the protos
-    # definitions, we shift the existing protos and then remove them
     with open(header_filepath, "r+") as f:
         content = f.readlines()
-        # insert new protos
-        content[first_header_proto_idx:first_header_proto_idx] = protos
 
-        # compute old protos position and remove
-        old_protos_start = first_header_proto_idx + len(protos)
-        old_protos_end = old_protos_start + nb_old_protos
+        # insert new prototypes after the #define
+        insert_position = (
+            next((i for i, line in enumerate(content) if line.strip().startswith("#define")), 0) + 1
+        )
 
-        del content[old_protos_start:old_protos_end]
+        # add newline after #define if it doesn't exist
+        if insert_position > 0 and not content[insert_position - 1].strip() == "":
+            content.insert(insert_position, "\n")
+            insert_position += 1
+
+        # remove existing protos
+        start_remove = insert_position
+        end_remove = next(
+            (
+                i
+                for i, line in enumerate(content[insert_position:], start=insert_position)
+                if line.strip().startswith("#endif")
+            ),
+            len(content),
+        )
+        del content[start_remove:end_remove]
+
+        # insert new prototypes
+        content[insert_position:insert_position] = protos
+
+        # newline before #endif
+        if content[insert_position + len(protos)].strip().startswith("#endif"):
+            content.insert(insert_position + len(protos), "\n")
+
         f.seek(0)
         f.writelines(content)
         f.truncate()
@@ -200,6 +242,15 @@ def generate_prototypes(
                 raise ValueError("A header file already exists at provided location.")
 
             header_visitor = _visit_file(header_filepath)
+
+            # check if the header is correct
+            # else create a correct structure
+            _validate_header_structure(
+                header_filepath=header_filepath,
+                source_filepath=source_filepath,
+                existing_prototypes=header_visitor.protos,
+            )
+
             _update_header(
                 header_filepath=header_filepath,
                 header_visitor=header_visitor,
